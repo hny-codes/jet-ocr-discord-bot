1552cf7135eaecbefe0b574aa0e111632594f55f
Henry <hnguyhenry@protonmail.com>
2023-11-30 00:55:23 -0800

    Add help command
    Override basic help command with a custom help command. Small descriptions are also editted

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from image_util import parse_image
 from os.path import join, dirname
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord.ext.commands import CommandNotFound
 
 # Load environmental variables
 load_dotenv()
@@ -19,8 +20,9 @@ intents.message_content = True
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
 # Bot utility
-bot_desc = 'Test bot'
+bot_desc = 'Taking the text out of your images, done easy!'
 bot = commands.Bot(command_prefix='$', description=bot_desc, intents=intents)
+bot.remove_command('help')
 
 # On Ready Event
 @bot.event
@@ -34,8 +36,23 @@ async def hello(ctx):
   """ Basic command that says hi back """
   await ctx.send('Hi!')
 
+@bot.event
+async def on_command_error(ctx, error):
+  """ Catch non-existing bot commands """
+  if isinstance(error, CommandNotFound):
+    await ctx.send('Command doesn\'t exist!')
+
+@bot.command()
+async def help(ctx):
+  """Display a list of available commands to use"""
+  help_text = f"""
+  ```{bot_desc}\n\nCommands\n\t$text - I will take in an image and grab the text from it for you!\n\n👀Don\'t forget to attach an image along with this command! I can read even Japanese!\n\nExample: $text ja```"""
+  
+  await ctx.send(help_text.strip())
+
 @bot.command()
 async def text(ctx, lang='en'):
+  """ Takes in an image and returns text """
   try:
     attachment = ctx.message.attachments[0]
     text = parse_image(attachment, lang)
