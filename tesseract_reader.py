041e65ab9a90d7dd60070915bad76def012ba79b
hny-codes <hnguyhenry@protonmail.com>
2023-12-02 16:36:28 -0800

    Add TesseractReader object

---
+++ tesseract_reader.py
@@ -0,0 +1,26 @@
+import requests
+import io
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+import cv2
+
+
+class TesseractReader:
+    def __init__(self):
+        print('Initiated Tesseract Reader')
+
+    def parse_image(self, image):
+        try:
+            tessdata_dir_config = r'-l eng+jpn --tessdata-dir "/home/tachi/Desktop/langs/tessdata"'
+            print("Parsing via pytesseract")
+
+            return pytesseract.image_to_string(
+                image=image, timeout=120, config=tessdata_dir_config
+            )
+
+        except RuntimeError as e:
+            print(e)
