ce653ee7c9fdd0c282ba152727d0d4f139645996
hny-codes <hnguyhenry@protonmail.com>
2023-12-02 16:36:05 -0800

    Remove need of lang specification for jpn

--- image_util.py
+++ image_util.py
@@ -2,25 +2,27 @@ import requests
 import io
 from ocr_reader import ImageReader
 from tesseract_reader import TesseractReader
+
 try:
-  from PIL import Image
+    from PIL import Image
 except ImportError:
-  import Image
+    import Image
 
 
 def get_image(url):
-  """ Parses an image URL and returns an PIL image """
-  response = requests.get(url)
-  image = Image.open(io.BytesIO(response.content))
+    """Parses an image URL and returns an PIL image"""
+    response = requests.get(url)
+    image = Image.open(io.BytesIO(response.content))
 
-  return image
+    return image
 
-def parse_image(img, lang):
-  """ Grabs the text of a given image and returns it """
-  response = requests.get(img)
-  image = Image.open(io.BytesIO(response.content))
-  print('Image: ', image)
 
-  reader = TesseractReader()
-  text = f'```{reader.parse_image(image)}```'
-  return text
+def parse_image(img):
+    """Grabs the text of a given image and returns it"""
+    response = requests.get(img)
+    image = Image.open(io.BytesIO(response.content))
+    print("Image: ", image)
+
+    reader = TesseractReader()
+    text = f"```{reader.parse_image(image)}```"
+    return text
