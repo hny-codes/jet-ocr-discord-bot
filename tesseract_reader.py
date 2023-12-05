4ba8543cc70b302677796fb6cbb407f7aef36348
hny-codes <hnguyhenry@protonmail.com>
2023-12-04 18:49:17 -0800

    Add OSD check to determine language
    Add function to determine language to be OCR'ed utilizing tesseract's image_to_osd function. If given image is not english or japanese, then a catchall will be performed instead, trying to grab as much text as possible

--- tesseract_reader.py
+++ tesseract_reader.py
@@ -11,16 +11,57 @@ import cv2
 
 class TesseractReader:
     def __init__(self):
-        print('Initiated Tesseract Reader')
+        print("Initiated Tesseract Reader")
+        # Set psm to single column of text 
+        self.psm = 4
+
+    def get_language(self, image):
+        """Get OSD detected language of image"""
+        print("Getting language..")
+
+        # Reduce minimum characters of target image to determine language
+        # REFERENCE: https://stackoverflow.com/a/68978474
+        config = r"--psm 0 -c min_characters_to_try=5"
+        try:
+            info = pytesseract.image_to_osd(image=image, config=config)
+            print("Info: ", info)
+            return info.split("\n")[-3].split()[1]
+        except Exception as e:
+            print("Error: ", e)
+
+            # Set PSM to bypass tesseract segmentation, catch all
+            self.psm = 3
+            return "CatchAll"
 
     def parse_image(self, image):
         try:
-            tessdata_dir_config = r'--tessdata-dir "/home/tachi/Desktop/langs/tessdata"'
+            text = ""
+
             print("Parsing via pytesseract")
 
-            return pytesseract.image_to_string(
-                image=image, timeout=120, config=tessdata_dir_config, lang='eng+jpn'
+            # Get language of image
+            language = self.get_language(image)
+            print("Language: ", language, "\n")
+
+            tessdata_dir_config = (
+                f'--tessdata-dir "/home/tachi/Desktop/langs/tessdata" --psm {self.psm}'
             )
+
+            if language == "Japanese" or language == "Katakana":
+                text = pytesseract.image_to_string(
+                    image=image, timeout=120, config=tessdata_dir_config, lang="jpn"
+                )
+            elif language == "Latin" or language == "Cyrillic":
+                text = pytesseract.image_to_string(
+                    image=image, config=tessdata_dir_config
+                )
+            else:
+                catch_config = f'--tessdata-dir "/home/tachi/Desktop/langs/tessdata" -l jpn+eng --psm {self.psm}'
+                text = pytesseract.image_to_string(
+                    image=image, config=tessdata_dir_config
+                )
+
+            return text
 
         except RuntimeError as e:
             print(e)
